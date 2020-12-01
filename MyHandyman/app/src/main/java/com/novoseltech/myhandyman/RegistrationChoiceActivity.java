0c26735c0ba2f7654f7c49298e6cefd4fec7193e
denisnovosel <35872964+denisnovosel@users.noreply.github.com>
2020-12-01 16:14:02 +0000

    Add
    Initial project files

---
+++ MyHandyman/app/src/main/java/com/novoseltech/myhandyman/RegistrationChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.novoseltech.myhandyman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_choice);
+    }
+}
