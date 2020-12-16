71cc063df467a85c6776395cbce634f2f7b6d94e
denisnovosel <35872964+denisnovosel@users.noreply.github.com>
2020-12-16 22:06:45 +0000

    ADD
    New activities

--- MyHandyman/app/src/main/java/com/novoseltech/myhandyman/HomeActivity.java
+++ MyHandyman/app/src/main/java/com/novoseltech/myhandyman/HomeActivity.java
@@ -2,13 +2,60 @@ package com.novoseltech.myhandyman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Functions func = new Functions();
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+    }
+
+    public void ClickLogOut(View view){
+
+        //Close app
+        //Initialize alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //Set title
+        builder.setTitle("Log out");
+        //Set message
+        builder.setMessage("Are you sure you want to log out ?");
+        //Yes button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                func.redirectActivity(HomeActivity.this, MainActivity.class);
+
+            }
+        });
+
+        //No button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Dismiss dialog
+                dialogInterface.dismiss();
+            }
+        });
+        //Show dialog
+        builder.show();
     }
 }
